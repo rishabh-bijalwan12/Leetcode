b27c9d8ba9ef9e9a0f5cfaa43e751200f1d107b7
Rishabh Bijalwan <116650217+rishabh-bijalwan12@users.noreply.github.com>
2024-09-10 21:25:13 +0530

    Time: 11 ms (57.60%), Space: 14.1 MB (77.45%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[1001][101][2];
+    int solve(vector<int>&p,int k,int i,bool buy){
+        if(i>=p.size())return 0;
+        if(k==0)return 0;
+        
+        if(dp[i][k][buy]!=-1)return dp[i][k][buy];
+
+        int ans=INT_MIN;
+
+        if(buy==false){
+            ans=max(solve(p,k,i+1,buy),-p[i]+solve(p,k,i+1,!buy));
+        }
+        else{
+            ans=max(solve(p,k,i+1,buy),p[i]+solve(p,k-1,i+1,!buy));
+        }
+        return dp[i][k][buy]=ans;
+    }
+    int maxProfit(int k, vector<int>& p) {
+        memset(dp,-1,sizeof(dp));
+        return solve(p,k,0,false);
+    }
+};
